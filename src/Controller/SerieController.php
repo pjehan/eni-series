9e2b99d09448545382469cca7bd322100eb990ca
pjehan <pierre.jehan@gmail.com>
2022-10-19 11:55:37 +0200

    Création du controlleur des séries

---
+++ src/Controller/SerieController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/series')]
+class SerieController extends AbstractController
+{
+    #[Route('/', name: 'series_list')]
+    public function list(): Response
+    {
+        return $this->render('serie/index.html.twig');
+    }
+
+    #[Route('/{id}', name: 'series_detail', requirements: ['id' => '\d+'])]
+    public function detail(): Response
+    {
+        return $this->render('serie/detail.html.twig');
+    }
+
+    #[Route('/new', name: 'series_new')]
+    public function new(): Response
+    {
+        return $this->render('serie/new.html.twig');
+    }
+}
