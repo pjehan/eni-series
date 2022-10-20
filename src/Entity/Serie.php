0c3444dacc7d7382f6a4e83af8a034d74aa1f035
pjehan <pierre.jehan@gmail.com>
2022-10-20 10:24:33 +0200

    Création de l'entité Serie

---
+++ src/Entity/Serie.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SerieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SerieRepository::class)]
+class Serie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $overview = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    #[ORM\Column]
+    private ?float $vote = null;
+
+    #[ORM\Column]
+    private ?float $popularity = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $genres = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $firstAirDate = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $lastAirDate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $backdrop = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $poster = null;
+
+    #[ORM\Column]
+    private ?int $tmdbId = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateCreated = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateModified = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOverview(): ?string
+    {
+        return $this->overview;
+    }
+
+    public function setOverview(string $overview): self
+    {
+        $this->overview = $overview;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getVote(): ?float
+    {
+        return $this->vote;
+    }
+
+    public function setVote(float $vote): self
+    {
+        $this->vote = $vote;
+
+        return $this;
+    }
+
+    public function getPopularity(): ?float
+    {
+        return $this->popularity;
+    }
+
+    public function setPopularity(float $popularity): self
+    {
+        $this->popularity = $popularity;
+
+        return $this;
+    }
+
+    public function getGenres(): ?string
+    {
+        return $this->genres;
+    }
+
+    public function setGenres(string $genres): self
+    {
+        $this->genres = $genres;
+
+        return $this;
+    }
+
+    public function getFirstAirDate(): ?\DateTimeInterface
+    {
+        return $this->firstAirDate;
+    }
+
+    public function setFirstAirDate(\DateTimeInterface $firstAirDate): self
+    {
+        $this->firstAirDate = $firstAirDate;
+
+        return $this;
+    }
+
+    public function getLastAirDate(): ?\DateTimeInterface
+    {
+        return $this->lastAirDate;
+    }
+
+    public function setLastAirDate(\DateTimeInterface $lastAirDate): self
+    {
+        $this->lastAirDate = $lastAirDate;
+
+        return $this;
+    }
+
+    public function getBackdrop(): ?string
+    {
+        return $this->backdrop;
+    }
+
+    public function setBackdrop(string $backdrop): self
+    {
+        $this->backdrop = $backdrop;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(string $poster): self
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getTmdbId(): ?int
+    {
+        return $this->tmdbId;
+    }
+
+    public function setTmdbId(int $tmdbId): self
+    {
+        $this->tmdbId = $tmdbId;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(\DateTimeInterface $dateCreated): self
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    public function getDateModified(): ?\DateTimeInterface
+    {
+        return $this->dateModified;
+    }
+
+    public function setDateModified(?\DateTimeInterface $dateModified): self
+    {
+        $this->dateModified = $dateModified;
+
+        return $this;
+    }
+}
