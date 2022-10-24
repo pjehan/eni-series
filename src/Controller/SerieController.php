8df98c94de085a71963479fe257e78579207648c
pjehan <pierre.jehan@gmail.com>
2022-10-24 14:49:41 +0200

    Affichage du formulaire de création d'une série

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Form\SerieType;
 use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,8 +39,29 @@ class SerieController extends AbstractController
     }
 
     #[Route('/new', name: 'series_new')]
-    public function new(): Response
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('serie/new.html.twig');
+        $serie = new Serie();
+        $serie->setDateCreated(new \DateTime()); // Ou utiliser les LifeCycleCallbacks de Doctrine
+        $serieForm = $this->createForm(SerieType::class, $serie);
+
+        // Récupération des données pour les insérer dans l'objet $serie
+        $serieForm->handleRequest($request);
+
+        // Vérifier si l'utilisateur est en train d'envoyer le formulaire
+        if ($serieForm->isSubmitted()) {
+            // Enregistrer la nouvelle série en BDD
+            $em->persist($serie);
+            $em->flush();
+
+            $this->addFlash('success', 'La série a bien été créée !');
+
+            // Rediriger l'internaute vers la liste des séries
+            return $this->redirectToRoute('series_list');
+        }
+
+        return $this->render('serie/new.html.twig', [
+            'serieForm' => $serieForm->createView()
+        ]);
     }
 }
--- src/Form/SerieType.php
+++ src/Form/SerieType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Serie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +17,30 @@ class SerieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('overview')
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['class' => 'input-serie-name']
+            ])
+            ->add('overview', TextareaType::class, [
+                'label' => 'Résumé'
+            ])
             ->add('status')
-            ->add('vote')
-            ->add('popularity')
+            ->add('vote', NumberType::class, [
+                'html5' => true
+            ])
+            ->add('popularity', NumberType::class, [
+                'html5' => true
+            ])
             ->add('genres')
-            ->add('firstAirDate')
-            ->add('lastAirDate')
-            ->add('backdrop')
-            ->add('poster')
+            ->add('firstAirDate', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('lastAirDate', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('backdrop', FileType::class)
+            ->add('poster', FileType::class)
             ->add('tmdbId')
-            ->add('dateCreated')
-            ->add('dateModified')
         ;
     }
 
